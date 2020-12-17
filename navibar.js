b7171c7e49af9d09c07561bfaf1073a508d69a84
Marina294 <69162947+Marina294@users.noreply.github.com>
2020-12-17 12:35:12 -0800

    added index css

--- navibar.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $(window).scroll(function() {
-      if ($(this).scrollTop() > 0) {
-        $('navbar').css('background-color', '#FFFFFF');
-      } else {
-        $('navbar').css('background', 'none');
-      }
-    });
-  });
